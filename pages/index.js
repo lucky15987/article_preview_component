d5e9f5866c5bc855e1df2f0799aa3ea4ce908e09
Ethan N <lucky35789@yahoo.com>
2022-08-29 12:15:07 -0400

    added image properties

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="px-8 mt-10">
       <Head>
-          <title>Article Preview Component</title>
-          <link rel="icon" href="/favicon.ico"  />   
+        <title>Article Preview Component</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div className="mx-auto">
+        <img className="rounded-tr-md rounded-tl-md h-48 w-full" src='/images/drawers.jpg' alt='' />
+      </div>
     </div>
   )
 }
