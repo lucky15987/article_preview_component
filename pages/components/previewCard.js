daf9bf6d808784ae11f0537fc753437c4e553cef
Ethan N <lucky35789@yahoo.com>
2022-08-29 16:22:34 -0400

    added 404 page

---
+++ pages/components/previewCard.js
@@ -0,0 +1,10 @@
+
+
+export default function PreviewCard() {
+    return (
+       <div className="h-32 w-96 bg-gray-300 relative bottom-44 inset-x-2/4 rounded-md opacity-full">
+            
+       </div>
+    );
+}
+
