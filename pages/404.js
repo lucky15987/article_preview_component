6227769dc339123b1eb302c042951500bba93f36
Ethan N <lucky35789@yahoo.com>
2022-08-29 16:23:01 -0400

    adjusted elements for additional components

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return (
+        <div className="bg-black h-screen w-full">
+            <p>Error 404!</p>
+            <p>This page does not exist!</p>
+        </div>
+    );
+}
+
--- pages/index.js
+++ pages/index.js
@@ -19,14 +19,16 @@ export default function Home() {
                 <p className='text-gray-600 text-sm'>28 Jun 2020</p>
               </div>
             </div>
-            <button href="#" className="w-8 h-8 ml-auto bg-gray-200 rounded-full flex items-center justify-center hover:bg-gray-300 ">
+            <button href="#" className="w-8 h-8 ml-auto bg-gray-200 rounded-full flex items-center justify-center hover:bg-gray-300">
               <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13">
                 <path fill="#6E8098" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z" />
               </svg>
+            
             </button>
           </div>
-        </div>
+        </div>        
       </div>
+      
     </div>
   )
 }
