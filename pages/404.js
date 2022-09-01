abe2abb84f1a01f528e2860ebf2fe490d656f5e3
Ethan N <lucky35789@yahoo.com>
2022-09-01 14:20:32 -0400

    adjusted navbar

--- pages/404.js
+++ pages/404.js
@@ -3,12 +3,14 @@ import Navbar from "./components/navbar";
 export default function Custom404() {
     return (
         <>
-            <div className="bg-black h-screen w-full">
-                {/* <p>Error 404!</p>
-                <p>This page does not exist!</p> */}
-                <Navbar/>
+            <div className="bg-black h-screen w-full flex flex-row">
+                <div className="flex flex-col justify-start ml-20">
+                    <p>Error 404!</p>
+                    <p>This page does not exist!</p>
+                </div>
+                <Navbar />
             </div>
-            
+
         </>
 
     );
